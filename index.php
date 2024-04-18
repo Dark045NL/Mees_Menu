d47fe83cb49cc1a9cfd8eb0c5eec0c4147041b15
Dark045 <87452921+Dark045NL@users.noreply.github.com>
2024-04-19 01:56:56 +0200

    openingstijden

--- index.php
+++ index.php
@@ -90,6 +90,18 @@
             </div>
         </div>
     </div>
-
+<div id="opening_hours">
+    <h2>Opening Hours</h2>
+    <ul>
+        <li>Monday: 9:00 AM - 6:00 PM</li>
+        <li>Tuesday: 9:00 AM - 6:00 PM</li>
+        <li>Wednesday: 9:00 AM - 6:00 PM</li>
+        <li>Thursday: 9:00 AM - 6:00 PM</li>
+        <li>Friday: 9:00 AM - 8:00 PM</li>
+        <li>Saturday: 10:00 AM - 8:00 PM</li>
+        <li>Sunday: Closed</li>
+    </ul>
+</div>
+    
 </body>
 </html>
